d47734aa878da225228824764b80f49240813779
dmystical-coder <dmystical.coder@gmail.com>
2024-10-29 08:45:04 +0100

    feat: Add category highlights

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
 const close = document.querySelector('.close');
 const popup = document.querySelector('.popup-container');
+const menuIcon = document.querySelector('.menu-icon');
+const menuList = document.querySelector('.menu-list');
 
 close.addEventListener('click', () => {
     popup.style.display = 'none';
+});
+menuIcon.addEventListener('click', () => {
+    menuList.classList.toggle('hidden');
 })
